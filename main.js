979c0c56d02133e40a8b1270709213dac41527ab
BenAvrahamos <benavraham1998@gmail.com>
2024-02-13 18:31:49 +0200

    add ball increment size function by click

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 'use strict'
+function onBallClick(elBall) {
+    const currSize = elBall.offsetWidth
+    const newSize = elBall.offsetWidth + 50
 
-function onBallClick(){
-    console.log('hello');
+
+    elBall.style.width = newSize +'px'
+    elBall.style.height = newSize + 'px'
+
 }
